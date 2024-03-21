c21fdae969fb45cec3672a8de4e95e3e3c411bd6
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2024-03-21 12:20:38 +0000

    created a connection to existing Database
    I created a new connection to my existing database course_management_system with user and password. I created a table test_table to make sure it was connecting the right way. (I deleted the table inside mySQL database as it wasn't needed).

--- src/integrated/ca/IntegratedCA.java
+++ src/integrated/ca/IntegratedCA.java
@@ -14,7 +14,16 @@ public class IntegratedCA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // making sure that the console is working
         System.out.println("just checking");
+        
+        // create an instance of the DBConnector class
+        DBConnector db = new DBConnector();
+        
+        //create a fake table name to test the connection and call the method of DBConnector to creat this table.
+        String tableName = "test_table";
+        db.createTable(tableName);
+       
     }
     
 }
